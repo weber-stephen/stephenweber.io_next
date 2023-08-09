6ba5ba1310958e07d1ec38d3c7654bc73449b251
Stephen Weber <weber-stephen@users.noreply.github.com>
2023-08-09 14:26:48 -0700

    Adding GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 import React from 'react'
 
@@ -7,7 +8,21 @@ type AppLayoutProps = {
 }
 
 function MyApp({ Component, pageProps }: AppLayoutProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-LBN817RPZP" />
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-LBN817RPZP');
+        `}
+      </Script>
+    </>
+  )
 }
 
 export default MyApp
