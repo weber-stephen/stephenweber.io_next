424f42e965eb812cdee6efb57c3dbc79f3771f1d
Stephen Weber <weber-stephen@users.noreply.github.com>
2024-06-19 10:48:16 -0700

    Fixing build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  images: {
+      unoptimized: true
+  },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
